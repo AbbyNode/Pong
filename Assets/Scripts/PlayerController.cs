81c9fb931665339a0187cb715410840bc541f261
mshah155 <300759250@cencol.ca>
2017-06-26 17:34:45 -0400

    Added basic player movement
    Boundary not set

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// Public vars
+	public float speed;
+
+	// Private vars
+	private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start() {
+		rBody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		float vMove = Input.GetAxis("Vertical");
+		rBody.velocity = new Vector2(0, vMove) * speed;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
