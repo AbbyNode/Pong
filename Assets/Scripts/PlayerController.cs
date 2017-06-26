83942dd5c8fa70fc9667cffe3a5632d44b7a3a78
mshah155 <300759250@cencol.ca>
2017-06-26 18:13:19 -0400

    Player boundary, ball bounce
    Ball bounce doesn't work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary {
+	public float yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour {
 	// Public vars
+	public Boundary boundary;
 	public float speed;
 
 	// Private vars
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate() {
 		float vMove = Input.GetAxis("Vertical");
 		rBody.velocity = new Vector2(0, vMove) * speed;
+		rBody.position = new Vector2(rBody.position.x, Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax));
 	}
 
 	// Update is called once per frame
